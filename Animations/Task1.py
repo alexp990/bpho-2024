960db0eb36d8e74ce6312cd3b5cca6bc06fdd18b
alexp990 <a_pavlov23@wincoll.ac.uk>
2024-07-04 12:47:35 +0100

    Added Task1 initial animation idea

--- Animations/Task1.py
+++ Animations/Task1.py
@@ -2,105 +2,94 @@ from manim import *
 
 class ProjectileMotion(Scene):
     def construct(self):
-        # Constants
-        g = 9.8
-        u = 10  # Initial velocity
-        theta = 45 * DEGREES  # Angle of projection
-        h = 4  # Initial height
-        m = 1  # Mass, for completeness, though not used directly in plot
-        t_max = (u * np.sin(theta) + np.sqrt((u * np.sin(theta))**2 + 2 * g * h)) / g
-        x_max = u * np.cos(theta) * t_max
-        t_points = np.linspace(0, t_max, 100)
-        y = h + u * np.sin(theta) * t_points - 0.5 * g * t_points**2
-        x = u * np.cos(theta) * t_points
 
-        # Create graph
-        path = VMobject()
-        path.set_points_as_corners([*zip(x, y)])
-        path.set_stroke(color=BLUE)
+        h = 1  
+        u = 5
+        theta = 60  
+        dt = 0.05  
+        g = 9.81  
 
-        # Velocity vectors
-        def velocity_at(t):
-            vx = u * np.cos(theta)
-            vy = u * np.sin(theta) - g * t
-            return np.array([vx, vy, 0])
+        theta_rad = np.radians(theta)
 
-        def position_at(t):
-            px = u * np.cos(theta) * t
-            py = h + u * np.sin(theta) * t - 0.5 * g * t**2
-            return np.array([px, py, 0])
+        u_x = u * np.cos(theta_rad)
+        u_y = u * np.sin(theta_rad)
 
-        # Create dots and vectors
-        dot = Dot().move_to(position_at(0))
-        velocity_vector = Arrow(start=ORIGIN, end=velocity_at(0) / 3, color=RED)
-        vx_vector = Arrow(start=ORIGIN, end=velocity_at(0)[0] * RIGHT / 3, color=GREEN)
-        vy_vector = Arrow(start=ORIGIN, end=velocity_at(0)[1] * UP / 3, color=GREEN)
+        x_positions = []
+        y_positions = []
+        x_velocities = []
+        y_velocities = []
+        vc = []
+        t = 0
 
-        self.add(path)
-        self.add(dot)
-        self.add(velocity_vector)
-        self.add(vx_vector)
-        self.add(vy_vector)
+        while True:
+            t += dt
+            x = u_x * t
+            y = h + u_y * t - 0.5 * g * np.square(t)
+            vx = u_x
+            vy = u_y - g * t
+            v = np.sqrt(vx**2 + vy**2)
+            
+            if y < 0:
+                break
+            
+            x_positions.append(x)
+            y_positions.append(y)
+            x_velocities.append(vx)
+            y_velocities.append(vy)
+            vc.append(v)
 
-        # Animations
-        def update_dot(mob, dt):
-            t = self.time + dt
-            mob.move_to(position_at(t))
+        axes = Axes(
+            x_range=[0, max(x_positions) + 1, 1],
+            y_range=[0, max(y_positions) + 1, 1],
+            axis_config={"color": BLUE}
+        )
+        self.add(axes)
 
-        def update_velocity(mob, dt):
-            t = self.time + dt
-            mob.put_start_and_end_on(ORIGIN, velocity_at(t) / 3)
+        velocity_label = MathTex(r"u = 5 \, \text{m/s}")
+        angle_label = MathTex(r"\theta = 30^\circ")
+        
+        velocity_label.to_corner(UL).shift(RIGHT * 1)
+        angle_label.next_to(velocity_label, DOWN)
 
-        def update_vx(mob, dt):
-            t = self.time + dt
-            mob.put_start_and_end_on(ORIGIN, velocity_at(t)[0] * RIGHT / 3)
+        self.play(Write(velocity_label), Write(angle_label))
 
-        def update_vy(mob, dt):
-            t = self.time + dt
-            mob.put_start_and_end_on(ORIGIN, velocity_at(t)[1] * UP / 3)
+        projectile_path = axes.plot_line_graph(
+            x_values=x_positions,
+            y_values=y_positions,
+            line_color=RED
+        )
+        self.play(Create(projectile_path))
 
-        dot.add_updater(update_dot)
-        velocity_vector.add_updater(update_velocity)
-        vx_vector.add_updater(update_vx)
-        vy_vector.add_updater(update_vy)
+        i = 6
+        ball = Circle(radius=0.1, color=YELLOW).move_to(axes.coords_to_point(x_positions[i], y_positions[i]))
+        self.add(ball)
 
-        self.add(dot, velocity_vector, vx_vector, vy_vector)
+        mg_vector = Arrow(start=ball.get_center(), end=ball.get_center() + DOWN, color=GREEN)
+        vx_vector = Arrow(start=ball.get_center(), end=ball.get_center() + RIGHT * x_velocities[i], color=BLUE)
+        vy_vector = Arrow(start=ball.get_center(), end=ball.get_center() + UP * y_velocities[i], color=BLUE)
+        v_vector = Arrow(start=ball.get_center(), end=ball.get_center() + np.array([x_velocities[i], y_velocities[i], 0]), color=ORANGE)
 
-        # Annotations
-        self.play(FadeIn(Text("No air resistance", font_size=24).to_edge(UP + RIGHT)))
-        self.play(FadeIn(Text("i.e., an inverted parabolic trajectory", font_size=24, color=BLACK).to_edge(RIGHT)))
+        self.play(
+            Create(mg_vector),
+            Create(vx_vector),
+            Create(vy_vector),
+            Create(v_vector)
+        )
 
-        self.wait(t_max)
+        mg_label = MathTex(r"\vec{mg}").next_to(mg_vector, DOWN)
+        vx_label = MathTex(r"\vec{v}_x").next_to(vx_vector, RIGHT)
+        vy_label = MathTex(r"\vec{v}_y").next_to(vy_vector, UP)
+        v_label = MathTex(r"\vec{v}").next_to(v_vector, RIGHT)
 
-        # Clear updaters
-        dot.remove_updater(update_dot)
-        velocity_vector.remove_updater(update_velocity)
-        vx_vector.remove_updater(update_vx)
-        vy_vector.remove_updater(update_vy)
+        self.play(
+            Write(mg_label),
+            Write(vx_label),
+            Write(vy_label),
+            Write(v_label)
+        )
 
-        # Draw velocity vectors at a specific point
-        t_specific = t_max / 2  # example time for a specific point
-        self.play(dot.animate.move_to(position_at(t_specific)))
-        self.play(velocity_vector.animate.put_start_and_end_on(ORIGIN, velocity_at(t_specific) / 3))
-        self.play(vx_vector.animate.put_start_and_end_on(ORIGIN, velocity_at(t_specific)[0] * RIGHT / 3))
-        self.play(vy_vector.animate.put_start_and_end_on(ORIGIN, velocity_at(t_specific)[1] * UP / 3))
+        self.wait(5)
 
-        # Labels
-        v_label = MathTex("v", color=RED).next_to(velocity_vector.get_end(), UR, buff=0.1)
-        vx_label = MathTex("v_x", color=GREEN).next_to(vx_vector.get_end(), DOWN, buff=0.1)
-        vy_label = MathTex("v_y", color=GREEN).next_to(vy_vector.get_end(), LEFT, buff=0.1)
-        self.play(Write(v_label), Write(vx_label), Write(vy_label))
-
-        self.wait(2)
-
-        # Labels for initial conditions
-        u_vector = Arrow(start=ORIGIN, end=u * np.array([np.cos(theta), np.sin(theta), 0]) / 3, color=RED)
-        u_label = MathTex("u", color=RED).next_to(u_vector.get_end(), UR, buff=0.1)
-        self.play(FadeIn(u_vector), Write(u_label))
-
-        theta_arc = Arc(radius=1, start_angle=0, angle=theta, color=RED)
-        theta_label = MathTex("\\theta", color=RED).next_to(theta_arc, RIGHT, buff=0.1)
-        self.play(FadeIn(theta_arc), Write(theta_label))
-
-        self.wait(2)
-
+if __name__ == "__main__":
+    scene = ProjectileMotion()
+    scene.render()
