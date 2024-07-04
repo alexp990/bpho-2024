0b225eb4ca693186a5c7689f805a60b8ad353141
alexp990 <a_pavlov23@wincoll.ac.uk>
2024-07-04 18:02:07 +0100

    Created full animation for Task 1. please see and tell me if I need to add anything and if anything needs tweaking.

--- Animations/Task1.py
+++ Animations/Task1.py
@@ -3,6 +3,8 @@ from manim import *
 class ProjectileMotion(Scene):
     def construct(self):
 
+#-----------------------Projectile Motion Simulation--------------------
+
         h = 1  
         u = 5
         theta = 60  
@@ -38,13 +40,20 @@ class ProjectileMotion(Scene):
             y_velocities.append(vy)
             vc.append(v)
 
+#-----------------------Animation--------------------
+
+        #Draws Axes for graph
         axes = Axes(
             x_range=[0, max(x_positions) + 1, 1],
             y_range=[0, max(y_positions) + 1, 1],
             axis_config={"color": BLUE}
         )
+
+        
+        # Add grid and axes to the scene
         self.add(axes)
 
+        #Labels for initial conditions
         velocity_label = MathTex(r"u = 5 \, \text{m/s}")
         angle_label = MathTex(r"\theta = 30^\circ")
         
@@ -53,6 +62,7 @@ class ProjectileMotion(Scene):
 
         self.play(Write(velocity_label), Write(angle_label))
 
+        #Plots path
         projectile_path = axes.plot_line_graph(
             x_values=x_positions,
             y_values=y_positions,
@@ -60,15 +70,17 @@ class ProjectileMotion(Scene):
         )
         self.play(Create(projectile_path))
 
-        i = 6
+        i = 6 #Index of position to plot object on
         ball = Circle(radius=0.1, color=YELLOW).move_to(axes.coords_to_point(x_positions[i], y_positions[i]))
         self.add(ball)
 
+        #vectors for object
         mg_vector = Arrow(start=ball.get_center(), end=ball.get_center() + DOWN, color=GREEN)
         vx_vector = Arrow(start=ball.get_center(), end=ball.get_center() + RIGHT * x_velocities[i], color=BLUE)
         vy_vector = Arrow(start=ball.get_center(), end=ball.get_center() + UP * y_velocities[i], color=BLUE)
         v_vector = Arrow(start=ball.get_center(), end=ball.get_center() + np.array([x_velocities[i], y_velocities[i], 0]), color=ORANGE)
 
+        #Draws vectors
         self.play(
             Create(mg_vector),
             Create(vx_vector),
@@ -76,11 +88,13 @@ class ProjectileMotion(Scene):
             Create(v_vector)
         )
 
+        #Labels for vectors of object
         mg_label = MathTex(r"\vec{mg}").next_to(mg_vector, DOWN)
         vx_label = MathTex(r"\vec{v}_x").next_to(vx_vector, RIGHT)
         vy_label = MathTex(r"\vec{v}_y").next_to(vy_vector, UP)
         v_label = MathTex(r"\vec{v}").next_to(v_vector, RIGHT)
 
+        #Drws labels for vectors
         self.play(
             Write(mg_label),
             Write(vx_label),
@@ -88,7 +102,21 @@ class ProjectileMotion(Scene):
             Write(v_label)
         )
 
+        #Labels showing equations describing projectile motion
+        x_eq = MathTex("x = u_x t")
+        y_eq = MathTex("y = h + u_y t - \\frac{1}{2} g t^2")
+        vx_eq = MathTex("v_x = u_x")
+        vy_eq = MathTex("v_y = u_y - g t")
+        v_eq = MathTex("v = \\sqrt{v_x^2 + v_y^2}")
+
+        self.wait(2)
+
+        equations = VGroup(x_eq, y_eq, vx_eq, vy_eq, v_eq).arrange(DOWN, buff=0.5)
+        equations.to_corner(UR)
+        self.play(Write(equations))
+
         self.wait(5)
+
 
 if __name__ == "__main__":
     scene = ProjectileMotion()
